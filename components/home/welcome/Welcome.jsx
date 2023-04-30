821249d4aaf271fe5ce3fb84604f1133e6369467
TalhaAhmed36 <stalha247@gmail.com>
2023-05-01 03:24:32 +0500

    Did some progress on custom components and did UI chnages to make it look better

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import {useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity,Image,FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants';
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
+const router = useRouter();
+const [activeJobType, setActiveJobType] = useState("Full-time")
+
   return (
     <View>
-      <Text>Welcome</Text>
+<View style={styles.container}>
+  <Text style={styles.userName}> Hello Talhaverse</Text>
+  <Text style={styles.welcomeMessage}> Find your perfect job</Text>
+
+  <View style={styles.searchContainer}>
+    <View style={styles.searchWrapper}>
+      <TextInput 
+        style={styles.searchInput}   
+        value = ""
+        onChange={()=>{}}
+        placeholder = "What are you looking for?"
+        placeholderTextColor="#D3D3D3" 
+        />
+       </View>
+<TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+  <Image
+  source={icons.search}
+  resizeMode='contain'
+  style = {styles.searchBtnImage}
+  />
+</TouchableOpacity>
+ </View>
+
+ <View style={styles.tabsContainer}>
+  <FlatList 
+  data = {jobTypes}
+  renderItem={({item})=>(
+<TouchableOpacity style={styles.tab(activeJobType, item)}
+onPress={()=>{
+setActiveJobType(item);
+router.push(`/search/${item}`)
+}}
+>
+  <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+</TouchableOpacity>
+  )}
+  keyExtractor={item => item}
+  contentContainerStyle={{columnGap: SIZES.small}}
+  horizontal
+  />
+ </View>
+
+
+</View>
     </View>
   )
 }

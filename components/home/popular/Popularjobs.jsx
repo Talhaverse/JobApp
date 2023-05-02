615bc94f4bc5f566820eca50d0bcb856457357c2
TalhaAhmed36 <stalha247@gmail.com>
2023-05-03 02:02:55 +0500

    Popular jobs module done

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,11 +6,17 @@ import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import useFetch from '../../../hook/useFetch'
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const {data, isLoading,error} = useFetch (
+    'search', {
+     query: 'React developer' , 
+     num_pages: 1
+    })
+
+    console.log(data);
   return (
     <View style={styles.container}>
 <View style={styles.header}>
@@ -28,7 +34,7 @@ const Popularjobs = () => {
   <Text>Something Went Wrong</Text>
 ) : (
   <FlatList 
-  data = {[1,2,3,4,5,6]}
+  data = {data}
  renderItem={({item})=>(
   <PopularJobCard item = {item} />
  )}
